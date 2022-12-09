45f682e079fc2782ad626072a06ceee6307a4b79
gel97 <alangilan.rogel@gmail.com>
2022-12-09 09:32:05 +0800

    Add Roles Module

---
+++ 10-20-2022/DDNHRIS/Controllers/SPMS_RolesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DDNHRIS.Models.SPMS;
+
+namespace DDNHRIS.Content
+{
+    public class SPMS_RolesController : Controller
+    {
+        SPMSDBEntities _db = new SPMSDBEntities();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string nums = "0123456789";
+        Random random = new Random();
+        string[] quan = new string[5];
+
+
+        public ActionResult Roles()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult getRoles()
+        {
+
+            var roles = _db.tSPMS_Roles.ToList();
+            var menus = _db.tSPMS_Menu.ToList();
+            var rolesmenu = _db.vSPMS_RoleMenu.ToList();
+            return Json(new { roles, menus, rolesmenu }, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpPost]
+        public ActionResult saveMenu(List<vSPMS_RoleMenu> rolesMenu)
+        {
+
+            foreach (var i in rolesMenu)
+            {
+                var isExist = _db.tSPMS_RoleMenu.Where(a => a.recNo == i.recNo).FirstOrDefault();
+
+                if (i.recNo == 0) // SAVE
+                {
+                    var add = new tSPMS_RoleMenu()
+                    {
+                       rID = i.rID,
+                       mID = i.mID
+                    };
+                    _db.tSPMS_RoleMenu.Add(add);
+                }
+                else
+                {
+                    isExist.mID = i.mID;
+                }
+            }
+            _db.SaveChanges();
+
+            return Json(new { status = 1 }, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpPost]
+        public ActionResult removedMenu(int recNo)
+        {
+
+            var remove = _db.tSPMS_RoleMenu.Where(a => a.recNo == recNo).FirstOrDefault();
+            _db.tSPMS_RoleMenu.Remove(remove);
+            _db.SaveChanges();
+
+            return Json(new { status = 1 }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ 10-20-2022/DDNHRIS/Scripts/AngularJS/SPMS_UTILITY/Roles.js
@@ -0,0 +1,126 @@
+﻿app.controller("SPMS_Roles", function ($scope, $window, $http, filterFilter) {
+    var s = $scope;
+
+    s.rolesmenu = [];
+   
+    loadData();
+
+    function loadData() {
+        $http.get('../SPMS_Roles/getRoles').then(function (response) {
+
+            s.roles = response.data.roles;
+            s.roles_menu = response.data.rolesmenu;
+            s.menus = response.data.menus;
+
+            angular.forEach(s.roles_menu, function (rm, keyfirst) {
+                s.rolesmenu.push({
+                    recNo: rm.recNo, rID: rm.rID, mID: rm.mID, old_mID: rm.mID, mDescription: rm.mDescription, R_Description: rm.R_Description
+                });
+
+               
+
+            });
+            console.log("roles: ", s.roles);
+            console.log("rolesmenu: ", s.rolesmenu);
+            console.log("menus: ", s.menus);
+
+
+        }), function (err) {
+            alert(err);
+        }
+    }
+
+    s.addMenu = function (data) {
+        console.log("Menu", data)
+        s.rolesmenu.push({recNo: 0, rID: data.RID, R_Description: data.R_Description });
+        console.log("rolesmenu", s.rolesmenu);
+    }
+
+    s.saveMenu = function () {
+        console.log("rolesmenu: ", s.rolesmenu);
+        console.log("savemenu: ", s.savemenu);
+
+        if (s.savemenu.length > 0) {
+            $http.post('../SPMS_Roles/saveMenu', { rolesMenu: s.savemenu }).then(function (response) {
+                if (response.data.status == 1) {
+                    s.roles_menu = {};
+                    s.rolesmenu = [];
+                    s.savemenu = [];
+                    loadData();
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        iconColor: '#FFFFFF',
+                        color: '#FFFFFF',
+                        timerProgressBar: true,
+                        background: '#24a0ed',
+                        didOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    })
+
+                    Toast.fire({
+                        icon: 'success',
+                        title: "Saved Successfully!"
+                    })
+                }
+            }), function (err) {
+                alert(err);
+            }
+        }
+      
+    }
+
+
+    s.removedMenu = function (data) {
+        s.rolesmenu.splice(s.rolesmenu.findIndex(v => v.$$hashKey === data.$$hashKey), 1);
+        if (data.recNo > 0) {
+            $http.post('../SPMS_Roles/removedMenu', { recNo: data.recNo }).then(function (response) {
+               
+            }), function (err) {
+                alert(err);
+            }
+        }
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            iconColor: '#FFFFFF',
+            color: '#FFFFFF',
+            timerProgressBar: true,
+            background: '#24a0ed',
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: 'success',
+            title: "Removed Successfully!"
+        })
+
+        console.log("removedMenu", data)
+        
+    }
+    s.savemenu = [];
+    s.onChangeSelect = function (data) {
+       // s.savemenu.find(v => v.$$hashKey === data.$$hashKey).mID = data.mID;
+        s.isExist = s.savemenu.find(v => v.$$hashKey === data.$$hashKey);
+       
+        if (s.isExist) {
+            console.log("true")
+            s.isExist.mID = data.mID;
+        }
+        else {
+            s.savemenu.push(data);
+
+        }
+        console.log(" s.savemenu", s.savemenu)
+
+    }
+});
